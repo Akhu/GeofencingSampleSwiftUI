a7393a5a82cbf9c9324d3acd5f6cd70d9e86a758
Anthony Da Cruz <me@anthony-dacruz.com>
2021-08-09 17:02:34 +0200

    Initial Commit

---
+++ GeofencingSwiftUI/GeofencingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeofencingSwiftUIApp.swift
+//  GeofencingSwiftUI
+//
+//  Created by Anthony Da cruz on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct GeofencingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
